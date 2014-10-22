7e487ca90da8ec7834709c4303c324eb5f326874
freepander <freepander@163.com>
2014-10-22 21:07:51 +0800

    基本功能done

--- code.py
+++ code.py
@@ -7,14 +7,21 @@ import os,sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import web
-import time
+import datetime
 import simplejson
 
+from peewee import *
+
 cur_file_dir = os.path.split(os.path.realpath(__file__))[0]
-DATA_PATH = cur_file_dir + '/data/'
+
+db = MySQLDatabase(host = '127.0.0.1', user = 'root', passwd = 'root', database = 'do_list')
+
 TEMPLATE_PATH = cur_file_dir + '/templates/'
+
 urls = (
 		'/', 'List',
+		'/task/create', 'Task_create',
+		'/create_table', 'Util_createTable',
 		)
 
 app = web.application(urls, globals())
@@ -24,7 +31,31 @@ render = web.template.render(TEMPLATE_PATH, cache = False, globals={})
 class List:
 	def GET(self):
 		base_href = web.ctx.home+'\\'
-		return render.index(base_href)
+		task_list = Task.select()
+		return render.index(base_href, task_list)
+
+class Task_create:
+	def POST(self):
+		data = web.input()
+		task = Task()
+		task.content = data.get('content')
+		task.datetime = datetime.datetime.now()
+		task.save()
+		web.seeother('/')
+
+
+class Util_createTable:
+	def GET(self):
+		db.connect()
+		db.create_tables([Task])
+
+class BaseModel(Model):
+	class Meta:
+		database = db
+
+class Task(BaseModel):
+	content = CharField()
+	datetime = DateTimeField()
 
 
 if __name__ == '__main__':
